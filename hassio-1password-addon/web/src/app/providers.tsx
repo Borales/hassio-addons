dd66fd99fd432884dc1336e156418cf78b77dcda
Borales <bordun.alexandr@gmail.com>
2024-02-08 00:28:50 +0100

    Remove unused dependencies

--- hassio-1password-addon/web/src/app/providers.tsx
+++ hassio-1password-addon/web/src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/system';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
 import { useRouter } from 'next/navigation';
@@ -13,14 +12,11 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-  const [queryClient] = React.useState(() => new QueryClient());
   const router = useRouter();
 
   return (
     <NextUIProvider navigate={router.push}>
-      <QueryClientProvider client={queryClient}>
-        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-      </QueryClientProvider>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     </NextUIProvider>
   );
 }
