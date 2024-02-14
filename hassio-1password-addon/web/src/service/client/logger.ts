f13cb1e124add775e94fdd401b12b2c03793b78f
Borales <bordun.alexandr@gmail.com>
2024-02-14 20:52:24 +0100

    Add error log level to logger configuration

--- hassio-1password-addon/web/src/service/client/logger.ts
+++ hassio-1password-addon/web/src/service/client/logger.ts
@@ -6,7 +6,9 @@ const loggerFormat = format.printf(({ level, message, timestamp, ...rest }) => {
   return `${timestamp} [${level}] ${message} ${Object.keys(rest).length ? JSON.stringify(rest, null, 2) : ''}`;
 });
 
-const logLevel = ['debug', 'info'].includes(process.env.APP_LOG_LEVEL as string)
+const logLevel = ['debug', 'info', 'error'].includes(
+  process.env.APP_LOG_LEVEL as string
+)
   ? process.env.APP_LOG_LEVEL
   : 'debug';
 
