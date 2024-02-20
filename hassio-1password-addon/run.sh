6bba66652c69ecf771d862b1e30f5d9602f32c04
Borales <bordun.alexandr@gmail.com>
2024-02-20 21:12:26 +0100

    Update Dockerfile and run.sh

--- hassio-1password-addon/run.sh
+++ hassio-1password-addon/run.sh
@@ -3,6 +3,16 @@
 # Start the cron service
 crond
 
+CONFIG_PATH=/data/options.json
+
+# Home assistant config folder
+HA_CONFIG_FOLDER=/config
+
+OP_TTR="$(bashio::config 'checkIntervalMin')"
+OP_DB_URL="file:/data/store.db?connection_limit=1"
+OP_SERVICE_ACCOUNT_TOKEN="$(bashio::config 'serviceAccountToken')"
+APP_LOG_LEVEL="$(bashio::config 'logLevel')"
+
 # Run Prisma migrations
 cd /app && npm exec -- prisma migrate deploy
 
