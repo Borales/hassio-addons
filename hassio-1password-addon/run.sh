02bff93fee30c78660c210d630eba1c1d9aee081
Borales <1118933+Borales@users.noreply.github.com>
2026-02-08 22:55:15 +0100

    fix: update version to 0.1.3 in config.json and enhance data directory checks in run.sh

--- hassio-1password-addon/run.sh
+++ hassio-1password-addon/run.sh
@@ -16,9 +16,21 @@ OP_DB_URL="file:/data/onepassword_app_store.db?connection_limit=1"
 OP_SERVICE_ACCOUNT_TOKEN="$(bashio::config 'serviceAccountToken')"
 APP_LOG_LEVEL="$(bashio::config 'logLevel')"
 
+# Check if `/data` directory exists
+if [ ! -d /data ]; then
+  bashio::log.fatal "The /data directory does not exist. Please mount the directory and try again."
+  exit 1
+fi
+
+# Check if `/data` is writable
+if [ ! -w /data ]; then
+  bashio::log.fatal "The /data directory is not writable. Please check the permissions and try again."
+  exit 1
+fi
+
 # Run Prisma migrations
-bashio::log.info "Running Prisma migrations..."
-cd /app && npm exec -- prisma generate
+bashio::log.info "Running Prisma migrations on ${OP_DB_URL}..."
+
 cd /app && OP_DB_URL=${OP_DB_URL} npm exec -- prisma migrate deploy
 
 # run the web-app
