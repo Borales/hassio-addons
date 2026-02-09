c5e0a2969890f541070ec400f348f07b9785251f
Borales <1118933+Borales@users.noreply.github.com>
2026-02-09 01:58:01 +0100

    fix: update version to 0.1.7 in config.json, simplify Dockerfile node_modules copying, and adjust Prisma migration command in run.sh

--- hassio-1password-addon/run.sh
+++ hassio-1password-addon/run.sh
@@ -16,25 +16,13 @@ OP_DB_URL="file:/data/onepassword_app_store.db"
 OP_SERVICE_ACCOUNT_TOKEN="$(bashio::config 'serviceAccountToken')"
 APP_LOG_LEVEL="$(bashio::config 'logLevel')"
 
-# Check if `/data` directory exists
-if [ ! -d /data ]; then
-  bashio::log.fatal "The /data directory does not exist. Please mount the directory and try again."
-  exit 1
-fi
-
-# Check if `/data` is writable
-if [ ! -w /data ]; then
-  bashio::log.fatal "The /data directory is not writable. Please check the permissions and try again."
-  exit 1
-fi
-
 # Run Prisma migrations
 bashio::log.info "Running Prisma migrations on ${OP_DB_URL}..."
 bashio::log.info "Node version: $(node --version)"
 
 cd /app || exit 1
 
-if ! node node_modules/.bin/prisma migrate deploy; then
+if ! npm prisma migrate deploy; then
   bashio::log.fatal "Prisma migration failed. Check the logs above for details."
   exit 1
 fi
